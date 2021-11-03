d4b26e9727df8081891cd32640493a8bd74aaac7
Devin Mullin <devin.mullin90@gmail.com>
2021-11-03 17:57:29 -0500

    commit tree c0337c5919fa5e076df8a37807c375d18fb1cfbb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,31 @@
 class ItemsController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   def index
+    if params[:user_id]
+      user_item = User.find(params[:user_id])
+      items = user_item.items  
+    else
     items = Item.all
+    end 
     render json: items, include: :user
   end
+
+  def show 
+    item = Item.find(params[:id])
+    render json: item
+  end
+
+  def create 
+    user = User.find(params[:user_id])
+    item = user.items.create(params[:name, :description, :price])
+    render json: item, status: :created
+  end
+
+private
+
+def render_not_found_response(exception)
+  render json: {error: "#{exception.model} not found"}, status: :not_found
+end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,15 @@
 class UsersController < ApplicationController
 
+  def index
+    user = User.all
+    render json: user, include: :items
+  end
+
   def show
     user = User.find_by(id: params[:id])
     render json: user, include: :items
+  rescue ActiveRecord::RecordNotFound => e
+    render json: {error: e.record.errors.full_messages}, include: :unprocessable_entity
   end
 
 end
